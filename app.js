b32b552a3edbe0b32227e10b7eb8a4a23920fadd
go2023 <tgoncharenko2013@gmail.com>
2024-12-14 22:27:53 -0800

    My answer to lesson/week12 (Jobs API - part 2) assignment

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
+// app.js
+
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+// const rateLimiter = require('express-rate-limit');
+const rateLimiter = require('express-rate-limit').default || require('express-rate-limit');
+const xss = require('xss-clean');
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,8 +26,18 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+
+const apiRateLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(apiRateLimiter);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 // routes
 app.use('/api/v1/auth', authRouter);
