c283f80b3404bf9076cc99049629404aa4619c0b
tetyanago2023 <137588868+tetyanago2023@users.noreply.github.com>
2024-12-18 10:03:53 -0800

    My answer to lesson/week10 (Jobs API - part 2) assignment (#1)
    * My answer to lesson/week12 (Jobs API - part 2) assignment
    
    * URL on render.com was added
    
    * UI for '/' route was added
    
    * Swagger stuff was created
    
    * Swagger stuff was enhanced
    
    ---------
    
    Co-authored-by: go2023 <tgoncharenko2013@gmail.com>

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
+// app.js
+// https://jobs-api-tetyana.onrender.com
+
 require('dotenv').config();
 require('express-async-errors');
+
+// extra security packages
+const helmet = require('helmet');
+const cors = require('cors');
+// const rateLimiter = require('express-rate-limit');
+const rateLimiter = require('express-rate-limit').default || require('express-rate-limit');
+const xss = require('xss-clean');
+
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+// const swaggerDocument = YAML.load('./swagger_tutorial.yaml');
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -16,8 +33,24 @@ const jobsRouter = require('./routes/jobs');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+
+const apiRateLimiter = rateLimiter({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 100, // limit each IP to 100 requests per windowMs
+});
+
+app.use(apiRateLimiter);
 app.use(express.json());
-// extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
+
+app.get('/', (req, res) => {
+    res.send('<h1>Jobs API</h1><a href="/api-docs">Documentation</a>');
+});
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 // routes
 app.use('/api/v1/auth', authRouter);
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -5,25 +5,71 @@ const { StatusCodes } = require('http-status-codes')
 const { BadRequestError, NotFoundError } = require('../errors')
 
 const getAllJobs = async (req, res) => {
-    res.send('get all jobs');
+    const jobs = await Job.find({ createdBy: req.user.userId }).sort('createdAt')
+    console.log(jobs, req.user.userId)
+    res.status(StatusCodes.OK).json({ jobs, count: jobs.length })
 }
 
 const createJob = async (req, res) => {
     req.body.createdBy = req.user.userId
-    const job = await Job.create({...req.body})
+    const job = await Job.create(req.body)
+    console.log(job, req.user.userId)
     res.status(StatusCodes.CREATED).json({ job })
 }
 
 const getSingleJob = async (req, res) => {
-    res.send('get single job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req
+    const job = await Job.findOne({
+        _id: jobId,
+        createdBy: userId
+    })
+
+    if (!job) {
+        throw new NotFoundError(`no job with id : ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const updateJob = async (req, res) => {
-    res.send('update job');
+    const {
+        body: { company, position },
+        user: { userId },
+        params: { id: jobId },
+    } = req
+
+    if (company === '' || position === "") {
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+
+    const job = await Job.findOneAndUpdate({
+        _id: jobId,
+        createdBy: userId
+    },
+        req.body,
+        { new: true, runValidators: true }
+    )
+    if (!job) {
+        throw new NotFoundError(`no job with id : ${jobId}`)
+    }
+    res.status(StatusCodes.OK).json({ job })
 }
 
 const deleteJob = async (req, res) => {
-    res.send('delete job');
+    const {
+        user: { userId },
+        params: { id: jobId },
+    } = req
+    const job = await Job.findByIdAndRemove({
+        _id: jobId,
+        createdBy: userId
+    })
+    if (!job) {
+        throw new NotFoundError(`no job with id : ${jobId}`)
+    }
+    res.status(StatusCodes.OK).send('Job was deleted successfully')
 }
 
 module.exports = {
